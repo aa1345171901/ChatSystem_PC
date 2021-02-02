fba3cd22727f507ae6972431f24554ca3fbfa037
aa1345171901 <32924652+1345171901@users.noreply.github.com>
2021-02-02 15:48:01 +0800

    Revert "r"
    This reverts commit 5e518dc9a7bdc65f8db9181d9f1275d85160a90d.

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm()); 
+            Application.Run(new LoginForm());
         }
     }
 }
