97d96f5ceebbd3a125ee1097e566bff91e30b0ba
aa1345171901 <32924652+1345171901@users.noreply.github.com>
2021-02-17 17:18:17 +0800

    Revert "Revert "Revert "fix logic"""
    This reverts commit 325ebf4cc5275d1a5be40cfed97fe79974eb837a.

--- Manager/Request/AgreeAddFriendRequest.cs
+++ Manager/Request/AgreeAddFriendRequest.cs
@@ -36,14 +36,16 @@
             if (returnCode == ReturnCode.Fail)
             {
                 int friendId = int.Parse(strs[1]);
-                mainForm.ResultValue = "添加失败，服务器出错";
-                mainForm.IsShow = 2;
+                RequestForm request;
+                mainForm.UserRequestDict.TryGetValue(friendId, out request);
+                request.IsAgree = 2;
             }
             else
             {
                 int friendId = int.Parse(strs[1]);
-                mainForm.ResultValue = "添加成功";
-                mainForm.IsShow = 1;
+                RequestForm request;
+                mainForm.UserRequestDict.TryGetValue(friendId, out request);
+                request.IsAgree = 1;
             }
         }
 
